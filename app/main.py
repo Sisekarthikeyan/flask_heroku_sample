d741a4153ae7a6682cf0ade857632bb18a441ea2
Sisekarthikeyan <karthimembership@gmail.com>
2020-05-11 19:13:11 +0530

    Intial upload1

--- app/main.py
+++ app/main.py
@@ -8,9 +8,7 @@ CORS(app)
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def home_view():
     return "<h1>Succesfully deployed on Heroku</h1>"
 	
-if __name__ == "__main__":
-    app.run()
